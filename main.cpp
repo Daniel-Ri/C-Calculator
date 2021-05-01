e49e9291b9a74a048742def0727c0e32c9c5cdfc
Hadiid164 <zumarhadiid1@gmail.com>
2021-05-01 13:05:05 +0700

    Update main.cpp
    File awal

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 /*File utama program CPP Calculator*/
 #include <stdio.h>
-#include "tambah.h"
-#include "kurang.h"
-#include "kali.h"
-#include "bagi.h"
 
 int main() {
-    //kamus
-    float A, B, Hasil;
-    char  C;
-    //algoritma
-    printf("nilai pertama: ");
-    scanf("%f", &A);
-    printf("nilai kedua  : ");
-    scanf("%f", &B);
-    printf("Operasi: ( + - * / )");
-    scanf("%c", &C);
-    
-    if (C=="+"){
-        Hasil = tambah(A,B);
-    }else if(C=="-"){
-        Hasil = kurang(A,B);
-    }else if(C=="*"){
-        Hasil = kali(A,B);
-    }else if(C=="/"){
-        Hasil = bagi(A,B);
-    }else{
-        printf("input salah")
+    char Op;
+    double first, second ;
+    printf("Enter an operator (+, -, *,): " );
+    scanf("%c", &Op );
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second );
+
+    switch (Op) {
+
     }
 
     return 0;
